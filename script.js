d7d847f0a81fbe5a361d23f3027093d5090d13ab
Autoz <kuro123520@hotmail.com>
2021-07-29 14:20:22 +0700

    add hidden visible

--- script.js
+++ script.js
@@ -5,18 +5,30 @@ const length = document.getElementById('length')
 const color = document.querySelector('#color')
 const search = document.querySelector('#search')
 const reset = document.querySelector('#reset')
+const text = document.querySelector('#text')
+const divtext = document.querySelector('#divtext')
+const divsearch = document.querySelector('#divsearch')
+divtext.style.visibility = 'hidden'
+divsearch.style.visibility = 'hidden'
 
 // define more constants and variables here
 
 btn_toggle.onclick = () => {
-  // your code here
+  if(divtext.style.visibility === 'hidden' && divsearch.style.visibility === 'hidden'){
+    divtext.style.visibility = 'visible'
+    divsearch.style.visibility = 'visible'
+  }else{
+    divtext.style.visibility = 'hidden'
+    divsearch.style.visibility = 'hidden'
+  }
+
 }
 
 // more codes for Search and Reset buttons here
 search.onclick = () => {
-
+  
 }
 
 reset.onclick = () => {
-  
+
 }
